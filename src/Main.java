009e18217b55cebaf139957c5d016a44c62372a6
ArlBenedict <aencarguez89245@liceo.edu.ph>
2025-08-18 11:27:17 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         GradeCalculator calculator = new GradeCalculator();
 
-        String studentName = "Arl Benedict B. Encarguez";
+        String studentName = "John Smith";
         double[] grades = {85.5, 92.0, 85.5, 90.0};
 
         double average = calculator.calculateAverage(grades);
