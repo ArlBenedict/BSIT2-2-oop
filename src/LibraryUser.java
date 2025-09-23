3a48ec3cae45c3c8393a6e82cd7abd12c6050012
ArlBenedict <aencarguez89245@liceo.edu.ph>
2025-09-23 13:22:55 +0800

    Create LibraryUser.java

---
+++ src/LibraryUser.java
@@ -0,0 +1,7 @@
+public interface LibraryUser {
+    String getUserType();
+    void borrowMedia(Media media);
+    void returnMedia(Media media);
+    int getMaxBorrowLimit();
+    String getName();
+}
